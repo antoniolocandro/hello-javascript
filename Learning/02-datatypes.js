722d5cf340a8c99eaea0f73ce651a460bcfee564
Antonio Locandro <antoniolocandro@hotmail.com>
2024-09-12 18:01:18 -0600

    aprendiendo

---
+++ Learning/02-datatypes.js
@@ -0,0 +1,46 @@
+// Cadenas de texto (string)
+let myName = "Antonio Locandro"
+let alias = 'alocandro'
+let email = `alocandro@gmail.com`
+
+// Números (number)
+let age = 41 // Entero 
+let height = 1.69 // Decimal
+
+// Booleanos (boolean)
+let isTeacher = true
+let isStudent = false
+
+// Undefined 
+let undefinedValue
+//console.log (undefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+//BigInt
+let myBigInt = BigInt(31574897897849465651323564674897897946546)
+let myBigInt2 = 31574897897849465651323564674897897946546n
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
